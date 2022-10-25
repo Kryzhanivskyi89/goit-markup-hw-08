9f0b5e9fcb280dbbd1a030f347fee93bfeff8af9
Kryzhanivskyi89 <andrew.506@ukr.net>
2022-10-25 08:27:53 +0300

    2

--- JS/mobile-menu.js
+++ JS/mobile-menu.js
@@ -1,3 +1,9 @@
+<script
+      defer
+      src="https://cdnjs.cloudflare.com/ajax/libs/body-scroll-lock/3.1.5/bodyScrollLock.min.js"
+      integrity="sha512-HowizSDbQl7UPEAsPnvJHlQsnBmU2YMrv7KkTBulTLEGz9chfBoWYyZJL+MUO6p/yBuuMO/8jI7O29YRZ2uBlA=="
+      crossorigin="anonymous"
+    ></script>
 (() => {
   const mobileMenu = document.querySelector('.js-menu-container');
   const openMenuBtn = document.querySelector('.js-open-menu');
